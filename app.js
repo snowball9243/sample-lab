4286a5ed0420840abc429a74c63386743c08d8f5
Snowball <annduong5@gmail.com>
2020-09-28 03:19:11 +0000

    finished problem1

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 console.log("Write your code below!")
 
 function makeNegative(i){
-
+  console.log(-i)
 }
 
 function printAddition(a, b){
-
+   
 }
 
 //Write your test cases below this comment
 
+makeNegative(1) // -1
+makeNegative(2) // -2
 
 //do not editing anything below this comment.
 module.exports = {
